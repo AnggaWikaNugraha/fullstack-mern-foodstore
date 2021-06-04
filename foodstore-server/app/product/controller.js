8e08e1fc465609eca1debb20c5d93d332d2f4ac4
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 00:14:36 +0700

    handle image upload

--- foodstore-server/app/product/controller.js
+++ foodstore-server/app/product/controller.js
@@ -1,17 +1,77 @@
 // (1) import model `Product`
 const Product = require('./model');
+const config = require('../config');
+
+const fs = require('fs');
+const path = require('path');
 
 async function store(req, res, next) {
     try {
 
-        // (1) bungkus operasi sebelumnya di dalam try 
         let payload = req.body;
 
-        let product = new Product(payload);
+        if (req.file) {
+            // ambil file gambarnya
+            let tmp_path = req.file.path;
+            // ambil type gambarnya jgp, png, jpeg dll
+            let originalExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+            // bikin file dengan generat name dari multer 
+            let filename = req.file.filename + '.' + originalExt;
 
-        await product.save();
+            let target_path = path.resolve(config.rootPath, `public/upload/${filename}`);
 
-        return res.json(product);
+            // (1) baca file yang masih di lokasi sementara 
+            const src = fs.createReadStream(tmp_path);
+
+            // (2) pindahkan file ke lokasi permanen
+            const dest = fs.createWriteStream(target_path);
+
+            // (3) mulai pindahkan file dari `src` ke `dest`
+            src.pipe(dest);
+
+            src.on('end', async () => {
+
+                try {
+
+                    let product = new Product({ ...payload, image_url: filename })
+
+                    await product.save()
+
+                    return res.json(product);
+
+                } catch (err) {
+
+                    // (1) jika error, hapus file yang sudah terupload pada direktori
+                    fs.unlinkSync(target_path);
+
+                    // (2) cek apakah error disebabkan validasi MongoDB
+                    if (err && err.name === 'ValidationError') {
+                        return res.json({
+                            error: 1,
+                            message: err.message,
+                            fields: err.errors
+                        })
+                    }
+
+                    next(err);
+
+                }
+            });
+
+            src.on('error', async () => {
+
+                next(err);
+
+            });
+
+        } else {
+
+            let product = new Product(payload);
+
+            await product.save();
+
+            return res.json(product);
+        }
     } catch (err) {
         // ----- cek tipe error ---- //
         if (err && err.name === 'ValidationError') {
--- foodstore-server/app/product/router.js
+++ foodstore-server/app/product/router.js
@@ -3,12 +3,14 @@
 // (1) import router dari express
 const router = require('express').Router();
 const multer = require('multer');
+const os = require('os');
 
 // (2) import product controller 
 const productController = require('./controller');
 
 // (3) pasangkan route endpoint dengan method `store`
-router.post('/products', multer().none(), productController.store);
+// agar bisa menerima file upload dengan nama image
+router.post('/products', multer({ dest: os.tmpdir() }).single('image'), productController.store);
 
 // (4) export router 
 module.exports = router;
