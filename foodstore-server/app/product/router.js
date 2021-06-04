746f6dd887c187ea8948b03bac7414e59f42c8ae
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 00:36:18 +0700

    display list product using paginations limit skip

--- foodstore-server/app/product/router.js
+++ foodstore-server/app/product/router.js
@@ -8,6 +8,7 @@ const os = require('os');
 // (2) import product controller 
 const productController = require('./controller');
 
+router.get('/products', productController.index);
 // (3) pasangkan route endpoint dengan method `store`
 // agar bisa menerima file upload dengan nama image
 router.post('/products', multer({ dest: os.tmpdir() }).single('image'), productController.store);
