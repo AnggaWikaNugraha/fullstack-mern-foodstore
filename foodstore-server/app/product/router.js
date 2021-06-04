40352e0db451e8c151033621001ae45f09c81b86
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-04 23:49:32 +0700

    handle error

--- foodstore-server/app/product/router.js
+++ foodstore-server/app/product/router.js
@@ -2,12 +2,13 @@
 
 // (1) import router dari express
 const router = require('express').Router();
+const multer = require('multer');
 
 // (2) import product controller 
 const productController = require('./controller');
 
 // (3) pasangkan route endpoint dengan method `store`
-router.post('/products', productController.store);
+router.post('/products', multer().none(), productController.store);
 
 // (4) export router 
 module.exports = router;
