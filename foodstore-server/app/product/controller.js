cd5744c6f2c1d459de552aa69885b7505c0a2127
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-04 23:49:32 +0700

    handle error

--- foodstore-server/app/product/controller.js
+++ foodstore-server/app/product/controller.js
@@ -2,11 +2,28 @@
 const Product = require('./model');
 
 async function store(req, res, next) {
-    let payload = req.body;
-    // bikin baru
-    let product = new Product(payload)
-    await product.save()
-    return res.json(product)
+    try {
+
+        // (1) bungkus operasi sebelumnya di dalam try 
+        let payload = req.body;
+
+        let product = new Product(payload);
+
+        await product.save();
+
+        return res.json(product);
+    } catch (err) {
+        // ----- cek tipe error ---- //
+        if (err && err.name === 'ValidationError') {
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            });
+        }
+        // (2) tangkap jika terjadi kesalahan kemudian gunakan method `next` agar Express memproses error tersebut
+        next(err);
+    }
 }
 
 module.exports = {
