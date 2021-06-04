adcabb202161932acba2302e64c880825098a8ee
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 00:51:13 +0700

    controller delete dan router delete

--- foodstore-server/app/product/router.js
+++ foodstore-server/app/product/router.js
@@ -13,6 +13,7 @@ router.get('/products', productController.index);
 // agar bisa menerima file upload dengan nama image
 router.post('/products', multer({ dest: os.tmpdir() }).single('image'), productController.store);
 router.put('/products/:id', multer({ dest: os.tmpdir() }).single('image'), productController.update);
+router.delete('/products/:id', productController.destroy);
 
 // (4) export router 
 module.exports = router;
