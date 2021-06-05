f36f560288c58c57b28b0cc05ae887d48671b23d
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 12:26:23 +0700

    controller store category

---
+++ foodstore-server/app/category/controller.js
@@ -0,0 +1,34 @@
+const Category = require('./model');
+
+async function store(req, res, next) {
+    try {
+        // (1) tangkap payload dari _client request_ 
+        let payload = req.body;
+
+        // (2) buat category baru dengan model Category;
+
+        let category = new Category(payload);
+
+        // (3) simpan category baru tadi ke MongoDB
+        await category.save();
+
+        // (4) respon ke client dengan data category yang baru dibuat.
+        return res.json(category);
+
+    } catch (err) {
+        // ----- cek tipe error ---- //
+        if (err && err.name === 'ValidationError') {
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            });
+        }
+        // (2) tangkap jika terjadi kesalahan kemudian gunakan method `next` agar Express memproses error tersebut
+        next(err);
+    }
+}
+
+module.exports = {
+    store
+}
