a1509abaaddb73b906babd8cad56d85197455f4e
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 12:39:39 +0700

    router delete category

--- foodstore-server/app/category/router.js
+++ foodstore-server/app/category/router.js
@@ -10,6 +10,7 @@ const categoryController = require('./controller');
 // (4) endpoint untuk membuat kategori baru
 router.post('/categories', multer().none(), categoryController.store);
 router.put('/categories/:id', multer().none(), categoryController.update)
+router.delete('/categories/:id', categoryController.destroy);
 
 // (5) export router agar bisa dipakai di file `app.js`
 module.exports = router;
