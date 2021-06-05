41952f3fefce458beb9c141f5c50f3e27c49efd5
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 17:16:10 +0700

    endpoint login

--- foodstore-server/app/auth/contoller.js
+++ foodstore-server/app/auth/contoller.js
@@ -1,4 +1,9 @@
 const User = require('../user/model');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const config = require('../config');
+
 
 async function register(req, res, next) {
     try {
@@ -28,6 +33,54 @@ async function register(req, res, next) {
     }
 }
 
+// cocokkan email saat login
+async function localStrategy(email, password, done) {
+    try {
+        let user =
+            await User
+                .findOne({ email })
+                // pilih yg tidak mau di munculkan
+                .select('-__v -createdAt -updatedAt -cart_items -token');
+        if (!user) return done();
+        if (bcrypt.compareSync(password, user.password)) {
+            ({ password, ...userWithoutPassword } = user.toJSON());
+            return done(null, userWithoutPassword);
+        }
+    } catch (err) {
+        done(err, null)
+    }
+    done(); // <--- 
+}
+
+async function login(req, res, next) {
+    passport.authenticate('local', async function (err, user) {
+        if (err) return next(err);
+        if (!user) return res.json({ error: 1, message: 'email or password incorrect' }) // <-- 
+
+        // (1) buat JSON Web Token
+        let signed = jwt.sign(user, config.secretKey);
+        // (2) simpan token tersebut ke user terkait
+        await User.findOneAndUpdate({ _id: user._id }, {
+            $push: {
+                token:
+                    signed
+            }
+        }, { new: true });
+
+        // (3) response ke _client_ 
+        return res.json({
+            message: 'logged in successfully',
+            user: user,
+            token: signed
+        });
+
+
+    })(req, res, next);
+}
+
+
 module.exports = {
-    register
+    register,
+    localStrategy, // <---
+    login
 }
