23bedbf183231abc5b1ef8e3aa5579d168c19b9b
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 13:04:29 +0700

    controller , router update tag

--- foodstore-server/app/tag/controller.js
+++ foodstore-server/app/tag/controller.js
@@ -27,6 +27,31 @@ async function store(req, res, next) {
     }
 }
 
+async function update(req, res, next) {
+    try {
+        // (1) dapatkan data dari request yang dikirimkan client
+        let payload = req.body;
+
+        let tag = await Tag.findOneAndUpdate({ _id: req.params.id }, payload,
+            { new: true, runValidators: true });
+
+        // (4) respon ke client dengan data tag yang baru saja disimpan
+        return res.json(tag);
+
+    } catch (err) {
+        // (5) tangani kemungkinan error validasi dan error lainnya
+        if (err && err.name === 'ValidationError') {
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            });
+        }
+        next(err);
+    }
+}
+
 module.exports = {
-    store
+    store,
+    update
 }
--- foodstore-server/app/tag/router.js
+++ foodstore-server/app/tag/router.js
@@ -5,6 +5,7 @@ const tagController = require('./controller');
 
 // (4) buat route baru 
 router.post('/tags', multer().none(), tagController.store);
+router.put('/tags/:id', multer().none(), tagController.update);
 
 // (5) export router agar bisa digunakan di `app.js`
 module.exports = router;
