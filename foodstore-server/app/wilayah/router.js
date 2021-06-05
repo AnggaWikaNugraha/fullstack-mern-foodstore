a5aafdf547adb80703eca8a124d6f8a041bcfa96
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 22:15:29 +0700

    controller display kabupaten by provinsi

--- foodstore-server/app/wilayah/router.js
+++ foodstore-server/app/wilayah/router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const wilayahController = require('./controller');
 
 router.get('/wilayah/provinsi', wilayahController.getProvinsi);
+router.get('/wilayah/kabupaten', wilayahController.getKabupaten);
 
 module.exports = router;
