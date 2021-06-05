d49295d9da18971ac9c0056480ba8f784d2def3f
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 22:22:31 +0700

    router get kecamatan

--- foodstore-server/app/wilayah/router.js
+++ foodstore-server/app/wilayah/router.js
@@ -3,5 +3,6 @@ const wilayahController = require('./controller');
 
 router.get('/wilayah/provinsi', wilayahController.getProvinsi);
 router.get('/wilayah/kabupaten', wilayahController.getKabupaten);
+router.get('/wilayah/kecamatan', wilayahController.getKecamatan);
 
 module.exports = router;
