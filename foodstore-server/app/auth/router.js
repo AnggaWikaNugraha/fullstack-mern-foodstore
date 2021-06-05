b60cd044a381001457ba98ea8949910969c9b72b
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 17:16:10 +0700

    endpoint login

--- foodstore-server/app/auth/router.js
+++ foodstore-server/app/auth/router.js
@@ -1,9 +1,14 @@
 // (1) import package yang diperlukan 
 const router = require('express').Router();
 const multer = require('multer');
-// (2) import auth/controller.js
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const controller = require('./contoller');
+passport.use(new LocalStrategy({ usernameField: 'email' }, controller.localStrategy));
+
 // (3) buat endpoint untuk register user baru
 router.post('/register', multer().none(), controller.register);
+router.post('/login', multer().none(), controller.login)
+
 // (4) export router
 module.exports = router;
