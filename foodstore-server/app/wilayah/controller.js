ecd71a005098d0977d80744dcbd53a73a187c552
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-05 22:15:29 +0700

    controller display kabupaten by provinsi

--- foodstore-server/app/wilayah/controller.js
+++ foodstore-server/app/wilayah/controller.js
@@ -14,6 +14,27 @@ async function getProvinsi(req, res, next) {
     }
 }
 
+async function getKabupaten(req, res, next) {
+    const db_kabupaten = path.resolve(__dirname, './data/regencies.csv');
+    try {
+
+        let { kode_induk } = req.query;
+
+        const data = await csv().fromFile(db_kabupaten);
+
+        if (!kode_induk) return res.json(data);
+
+        return res.json(data.filter(kabupaten => kabupaten.kode_provinsi === kode_induk));
+
+    } catch (err) {
+        return res.json({
+            error: 1,
+            message: 'Tidak bisa mengambil data kabupaten, hubungi administrator'
+        });
+    }
+}
+
 module.exports = {
-    getProvinsi
+    getProvinsi,
+    getKabupaten
 }
