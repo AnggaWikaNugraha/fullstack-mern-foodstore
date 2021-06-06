e38a926ca57780c7865498f77e72149e1f0494c5
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-06 21:21:37 +0700

    provider store

--- foodstore-web/src/App.js
+++ foodstore-web/src/App.js
@@ -2,16 +2,19 @@ import logo from './logo.svg';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import 'upkit/dist/style.min.css'
 import Home from './pages/Home';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <Router>
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
       </Router>
-    </div>
+    </Provider>
+
   );
 }
 
