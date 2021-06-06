35334236073360cdf2404134b6c32b8ebdd8db15
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-06 21:59:23 +0700

    nilai null local storage

--- foodstore-web/src/features/Auth/reducer.js
+++ foodstore-web/src/features/Auth/reducer.js
@@ -1,6 +1,8 @@
 import { USER_LOGIN, USER_LOGOUT } from '../../app/constants';
 
-let initialState = { user: null, token: null }
+let initialState = localStorage.getItem('auth')
+    ? JSON.parse(localStorage.getItem('auth'))
+    : { user: null, token: null }
 
 export default function reducer(state = initialState, action) {
 
