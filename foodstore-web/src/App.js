d651e9129e3771b3bea82a65fcf70f967776eda8
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-06 21:59:23 +0700

    nilai null local storage

--- foodstore-web/src/App.js
+++ foodstore-web/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import 'upkit/dist/style.min.css'
 import Home from './pages/Home';
 import { Provider } from 'react-redux';
 import store from './app/store';
+// (1) import fungsi listen
+import { listen } from './app/listener';
+
 
 function App() {
+
+  // (2) panggil fungsi listen() sekali saja saat komponen selesai render pertama kali
+  React.useEffect(() => {
+    listen();
+  }, [])
+
+
   return (
     <Provider store={store}>
       <Router>
