7857d985a1dc409bfa89b8111ae0efc7bc06678d
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-06 15:48:41 +0700

    fixing controller update

--- foodstore-server/app/cart/controller.js
+++ foodstore-server/app/cart/controller.js
@@ -7,28 +7,25 @@ async function update(req, res, next) {
     let policy = policyFor(req.user);
 
     if (!policy.can('update', 'Cart')) {
+
         return res.json({
             error: 1,
             message: `You're not allowed to perform this action`
         });
+
     }
 
     try {
-        // (1) dapatkan `payload` `items`
+
         const { items } = req.body;
 
-        // (1) ekstrak `_id` dari masing-masing `item`
         const productIds = items.map(itm => itm._id);
 
-        // (1) cari data produk di MongoDB simpan sbg `products`
-        const products = await Product.find({ _id: { $in: productIds } })
+        const products =
+            await Product.find({ _id: { $in: productIds } })
 
         let cartItems = items.map(item => {
-
-            // (1) cari related product dari `products` berdasarkan `product._id` dan `item._id`
             let relatedProduct = products.find(product => product._id.toString() === item._id);
-
-            // (1) buat objek yang memuat informasi untuk disimpan sbg`CartItem`
             return {
                 _id: relatedProduct._id,
                 product: relatedProduct._id,
@@ -38,16 +35,12 @@ async function update(req, res, next) {
                 user: req.user._id,
                 qty: item.qty
             }
-
         });
 
         await CartItem.bulkWrite(cartItems.map(item => {
-
             return {
                 updateOne: {
-                    filter: {
-                        user: req.user._id, product: item.product
-                    },
+                    filter: { user: req.user._id, product: item.product },
                     update: item,
                     upsert: true
                 }
@@ -56,9 +49,10 @@ async function update(req, res, next) {
 
         return res.json(cartItems);
 
-    } catch (error) {
+    } catch (err) {
 
         // (1) tangani kemungkinan _error_ 
+
         if (err && err.name == 'ValidationError') {
             return res.json({
                 error: 1,
@@ -70,7 +64,10 @@ async function update(req, res, next) {
         next(err)
 
     }
+
 }
+
+
 
 async function index(req, res, next) {
 
