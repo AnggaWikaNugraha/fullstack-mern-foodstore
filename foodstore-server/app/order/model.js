7eb9d62bacef18ce0bcbb6afc33136ced24ce0a0
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-06 16:19:00 +0700

    controller order

--- foodstore-server/app/order/model.js
+++ foodstore-server/app/order/model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { model, Schema } = mongoose;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const orderSchema = Schema({
     status: {
@@ -25,5 +26,14 @@ const orderSchema = Schema({
     },
     order_items: [{ type: Schema.Types.ObjectId, ref: 'OrderItem' }]
 }, { timestamps: true });
+
+orderSchema.plugin(AutoIncrement, { inc_field: 'order_number' });
+
+orderSchema.virtual('items_count').get(function () {
+    return this.order_items.reduce((total, item) => {
+        return total +
+            parseInt(item.qty)
+    }, 0)
+});
 
 module.exports = model('Order', orderSchema);
