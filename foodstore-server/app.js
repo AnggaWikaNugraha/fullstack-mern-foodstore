6887db8261b5cd3aa26ac13aec1b462a8acd6ce5
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-06 16:39:38 +0700

    cors

--- foodstore-server/app.js
+++ foodstore-server/app.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use(decodeToken());
 app.use('/auth', authRouter);
 app.use('/api', productRouter);
