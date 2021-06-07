98f01854a5e034996d553938898479e4e445026e
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-08 01:08:29 +0700

    frontend -- register

--- foodstore-web/src/pages/Register/index.js
+++ foodstore-web/src/pages/Register/index.js
@@ -1,91 +1,93 @@
 import * as React from 'react';
-// (1) import LayoutOne
-import {
-    LayoutOne,
-    Card,
-    FormControl,
-    InputText,
-    InputPassword,
-    Button
-} from 'upkit';
+
+// (1) import InputPassword & Button
+import { LayoutOne, Card, FormControl, InputText, InputPassword, Button } from 'upkit';
+// (1) import useForm
 import { useForm } from 'react-hook-form';
-import { rules } from './validations';
+import { registerUser } from '../../api/auth';
 import { useHistory, Link } from "react-router-dom";
 const statuslist = {
-    idle: "idle",
-    process: "process",
-    success: "success",
-    error: "error",
-};
+    idle: 'idle',
+    process: 'process',
+    success: 'success',
+    error: 'error',
+}
 
 export default function Register() {
-
-    // (2) keluarkan fungsi yang diperlukan dari useForm
+    // (2) keluarkan fungsi `register`, `handleSubmit`, `errors`, `setError` dari `useForm`
     let { register, handleSubmit, errors, setError } = useForm();
+    // (2) state status dengan nilai default `statuslist.idle`
     let [status, setStatus] = React.useState(statuslist.idle);
-
+    let history = useHistory();
     // (1) buat fungsi untuk menangani form submit 
     const onSubmit = async formData => {
+        let respones = await registerUser(formData);
+        if (respones.status === 200) {
+            setStatus(statuslist.success)
+            // (1) redirect ke `register/berhasil`
+            history.push("/register/berhasil");
+        } else {
+            setStatus(statuslist.error)
+        }
     }
 
     return (
+
         <LayoutOne size="small">
+
             <Card color="white">
-                <div className="text-center mb-5">
-                    {/* <StoreLogo /> */}
-                </div>
+                {/* (4) gunakan onSubmit dengan terlebih dahulu dimasukan dalam handleSubmit */}
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <FormControl errorMessage={errors.full_name?.message}>
+
+                    {/* gunakan ref={register} */}
+                    <FormControl>
                         <InputText
                             name="full_name"
                             placeholder="Nama Lengkap"
                             fitContainer
-                            ref={register(rules.full_name)}
+                            {...register('full_name')}
                         />
                     </FormControl>
-                    <FormControl errorMessage={errors.email?.message}>
+
+                    {/* gunakan ref={register} */}
+                    <FormControl>
                         <InputText
                             name="email"
                             placeholder="Email"
                             fitContainer
-                            ref={register(rules.email)}
+                            {...register('email')}
                         />
                     </FormControl>
-                    <FormControl errorMessage={errors.password?.message}>
+
+                    {/* gunakan ref={register} */}
+                    <FormControl>
                         <InputPassword
                             name="password"
                             placeholder="Password"
                             fitContainer
-                            ref={register(rules.password)}
+                            {...register('password')}
                         />
                     </FormControl>
-                    <FormControl errorMessage={errors.password_confirmation?.message}>
+
+                    {/* gunakan ref={register} */}
+                    <FormControl>
                         <InputPassword
                             name="password_confirmation"
                             placeholder="Konfirmasi Password"
                             fitContainer
-                            ref={register(rules.password_confirmation)}
+                            {...register('password_confirmation')}
+
                         />
                     </FormControl>
+
                     <Button
                         size="large"
                         fitContainer
-                        disabled={status === statuslist.process}
-                    >
-                        {" "}
-                        {status === statuslist.process
-                            ? "Sedang memproses"
-                            : "Mendaftar"}{" "}
-                    </Button>
+                    > Mendaftar </Button>
+
                 </form>
             </Card>
-            <div className="text-center mt-2">
-                Sudah punya akun?{" "}
-                <Link to="/login">
-                    {" "}
-                    <b> Masuk Sekarang. </b>{" "}
-                </Link>
-            </div>
+
         </LayoutOne>
     )
 }
