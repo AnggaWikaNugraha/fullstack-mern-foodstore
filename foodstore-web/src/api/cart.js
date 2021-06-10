950c22ca7e267ffa71b6b70f7842f16bb35b534b
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-10 21:12:01 +0700

    get cart from server

--- foodstore-web/src/api/cart.js
+++ foodstore-web/src/api/cart.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import store from '../app/store';
 
 import { config } from '../config';
+import { setItems } from '../features/Cart/actions';
 
 export async function saveCart(token, cart) {
 
@@ -9,5 +11,26 @@ export async function saveCart(token, cart) {
             authorization: `Bearer ${token}`
         }
     })
+
+}
+
+export async function getCart() {
+
+    let { token } = localStorage.getItem('auth')
+        ? JSON.parse(localStorage.getItem('auth')) : {};
+
+    if (!token) return;
+
+    let { data } = await axios.get(`${config.api_host}/api/carts`, {
+
+        headers: {
+            authorization: `Bearer ${token}`
+        }
+
+    });
+
+    if (!data.error) {
+        store.dispatch(setItems(data));
+    }
 
 }
