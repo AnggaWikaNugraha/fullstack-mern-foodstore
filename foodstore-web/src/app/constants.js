0c1b6b37bb755d77e7bbc8f506f75559a7b14a87
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-10 20:11:13 +0700

    constrant cart state

--- foodstore-web/src/app/constants.js
+++ foodstore-web/src/app/constants.js
@@ -11,3 +11,8 @@ export const SET_TAGS_PRODUCT = 'features/Products/SET_TAGS';
 export const NEXT_PAGE_PRODUCT = 'features/Products/NEXT_PAGE';
 export const PREV_PAGE_PRODUCT = 'features/Products/PREV_PAGE';
 export const TOGGLE_TAG_PRODUCT = 'features/Products/TOGGLE_TAG';
+
+export const ADD_ITEM_CART = 'features/Cart/ADD_ITEM';
+export const REMOVE_ITEM_CART = 'features/Cart/REMOVE_ITEM';
+export const CLEAR_ITEMS_CART = 'features/Cart/CLEAR_ITEMS';
+export const SET_ITEMS_CART = 'features/Cart/SET_ITEMS';
