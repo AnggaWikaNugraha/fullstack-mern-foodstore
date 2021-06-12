46e3e5906f9329b27ca3549b890555b17de15d35
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-12 20:58:51 +0700

    component select wilayah

---
+++ foodstore-web/src/component/SelectWilayah/index.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import axios from 'axios';
+
+import { oneOf, number, oneOfType, string, func, shape } from 'prop-types';
+import { config } from '../../config';
+import { Select } from 'upkit';
+
+export default function SelectWilayah({ tingkat, kodeInduk, onChange }) {
+
+    // (1) definisikan _state_ lokal
+    let [data, setData] = React.useState([]);
+    let [isFetching, setIsFetching] = React.useState(false);
+
+    // (1) dapatkan data wilayah dari web API
+    React.useEffect(() => {
+
+        setIsFetching(true);
+
+        axios
+            .get(`${config.api_host}/api/wilayah/${tingkat}?kode_induk=${kodeInduk}`)
+            .then(({ data }) => setData(data))
+            .finally(_ => setIsFetching(false))
+
+    }, [kodeInduk, tingkat]);
+
+    // (1) gunakan komponen `<Select>` 
+    return <Select
+        options={data.map(wilayah => ({ label: wilayah.nama, value: wilayah.kode }))}
+        onChange={onChange}
+        value={value}
+        isLoading={isFetching}
+        isDisabled={isFetching || !data.length}
+    />
+
+}
+
+// (1) definisi propTypes
+SelectWilayah.propTypes = {
+    tingkat: oneOf(['provinsi', 'kabupaten', 'kecamatan', 'desa']),
+    kodeInduk: oneOfType([number, string]),
+    onChange: func, // (1)
+    value: shape({ label: string, value: oneOfType([string, number]) }) // (2)
+}
+
+SelectWilayah.defaultProps = {
+    tingkat: 'provinsi'
+}
