42bbae53752d199bdf22da878cf573bbcf84318a
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2021-06-13 09:55:23 +0700

    add confiigure db

--- foodstore-server/database/index.js
+++ foodstore-server/database/index.js
@@ -1,14 +1,21 @@
-// (1) import package mongoose
-const mongoose = require('mongoose');
-// (2) kita import konfigurasi terkait MongoDB dari `app/config.js`
-const { dbHost, dbName, dbPort, dbUser, dbPass } = require('../app/config');
-// (3) connect ke MongoDB menggunakan konfigurasi yang telah kitaimport 
-mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`,
-    {
-        useNewUrlParser: true, useUnifiedTopology: true,
-        useFindAndModify: false
-    });
-// (4) simpan koneksi dalam constant `db`
+// hp
+// const mongoose = require('mongoose');
+// const { dbHost, dbName, dbPort, dbUser, dbPass } = require('../app/config');
+// mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`,
+//     {
+//         useNewUrlParser: true, useUnifiedTopology: true,
+//         useFindAndModify: false
+//     });
+// const db = mongoose.connection;
+// module.exports = db;
+
+// rog
+const mongoose = require("mongoose");
+const { dbHost, dbName, dbPort, dbUser, dbPass } = require("../app/config");
+mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 const db = mongoose.connection;
-// (5) export `db` supaya bisa digunakan oleh file lain yang membutuhkan
 module.exports = db;
