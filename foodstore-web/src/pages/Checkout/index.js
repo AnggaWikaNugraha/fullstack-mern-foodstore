da516485a70f2084ed48f24d452e7103c8ff3cbb
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2021-06-13 10:51:31 +0700

    checkout : step 1 list items

--- foodstore-web/src/pages/Checkout/index.js
+++ foodstore-web/src/pages/Checkout/index.js
@@ -3,8 +3,13 @@ import TopBar from "../../component/Topbar";
 import FaCartPlus from "@meronex/icons/fa/FaCartPlus";
 import FaAddressCard from "@meronex/icons/fa/FaAddressCard";
 import FaInfoCircle from "@meronex/icons/fa/FaInfoCircle";
+import FaArrowRight from "@meronex/icons/fa/FaArrowRight";
 
-import { LayoutOne, Text, Steps } from "upkit";
+import { sumPrice } from "../../utils/sum-price";
+import { formatRupiah } from "../../utils/format-rupiah";
+import { config } from "../../config";
+import { useSelector } from "react-redux";
+import { LayoutOne, Text, Steps, Table, Button } from "upkit";
 
 const IconWrapper = ({ children }) => {
   return <div className="text-3xl flex justify-center">{children}</div>;
@@ -37,36 +42,70 @@ const steps = [
   },
 ];
 
+const columns = [
+  {
+    Header: "Nama produk",
+    accessor: (item) => (
+      <div className="flex items-center">
+        <img
+          src={`${config.api_host}/upload/${item.image_url}`}
+          width={48}
+          alt={item.name}
+        />
+        {item.name}
+      </div>
+    ),
+  },
+  {
+    Header: "Jumlah",
+    accessor: "qty",
+  },
+  {
+    Header: "Harga satuan",
+    id: "price",
+    accessor: (item) => <span> @ {formatRupiah(item.price)} </span>,
+  },
+  {
+    Header: "Harga total",
+    id: "subtotal",
+    accessor: (item) => {
+      return <div>{formatRupiah(item.price * item.qty)}</div>;
+    },
+  },
+];
+
 export default function Checkout() {
   let [activeStep, setActiveStep] = React.useState(0);
+
+  let cart = useSelector((state) => state.cart);
 
   return (
     <LayoutOne>
       <TopBar />
       <Text as="h3"> Checkout </Text>
       <Steps steps={steps} active={activeStep}></Steps>
-
-      <button onClick={(_) => setActiveStep(0)}> ke langkah pertama </button>
-      <button onClick={(_) => setActiveStep(1)}> ke langkah kedua </button>
-      <button onClick={(_) => setActiveStep(2)}> ke langkah ketiga </button>
-
-      <br />
-      <br />
-
-      <button onClick={(_) => setActiveStep(activeStep - 1)}>
-        {" "}
-        ke langkah sebelumnya{" "}
-      </button>
-      <button onClick={(_) => setActiveStep(activeStep + 1)}>
-        {" "}
-        ke langkah berikutnya{" "}
-      </button>
-
-      {activeStep === 0 ? <div> Langkah pertama </div> : null}
-
-      {activeStep == 1 ? <div> Langkah kedua </div> : null}
-
-      {activeStep == 2 ? <div> Langkah ketiga </div> : null}
+      {activeStep === 0 ? (
+        <div>
+          <br /> <br />
+          <Table
+            items={cart}
+            columns={columns}
+            perPage={cart.length}
+            showPagination={false}
+          />
+          <div className="text-right">
+            <Text as="h4">Subtotal: {formatRupiah(sumPrice(cart))}</Text>
+          </div>
+          <Button
+            onClick={(_) => setActiveStep(activeStep + 1)}
+            color="red"
+            iconAfter={<FaArrowRight />}
+          >
+            {" "}
+            Selanjutnya{" "}
+          </Button>
+        </div>
+      ) : null}
     </LayoutOne>
   );
 }
