cc8cb7a345feaf5c49ecea64e6bddbf73e129a69
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2021-06-13 10:26:37 +0700

    user id not found, while not add user when submit

--- foodstore-web/src/pages/userAddress/index.js
+++ foodstore-web/src/pages/userAddress/index.js
@@ -1,78 +1,63 @@
-import * as React from 'react';
-import TopBar from '../../components/TopBar';
+import * as React from "react";
+import TopBar from "../../component/Topbar";
 
-import { LayoutOne, Text, Table, Button } from 'upkit';
-import { Link } from 'react-router-dom';
-import { useAddressData } from '../../hooks/address';
+import { LayoutOne, Text, Table, Button } from "upkit";
+import { Link } from "react-router-dom";
+import { useAddressData } from "../../hooks/address";
 
 const columns = [
-    { Header: 'Nama', accessor: 'nama' },
-    {
-        Header: 'Detail', accessor: alamat => {
-            return <div>
-                {alamat.provinsi}, {alamat.kabupaten}, {alamat.kecamatan}, {alamat.kelurahan} <br />
-                {alamat.detail}
-            </div>
-        }
-    }
+  { Header: "Nama", accessor: "nama" },
+  {
+    Header: "Detail",
+    accessor: (alamat) => {
+      return (
+        <div>
+          {alamat.provinsi}, {alamat.kabupaten}, {alamat.kecamatan},{" "}
+          {alamat.kelurahan} <br />
+          {alamat.detail}
+        </div>
+      );
+    },
+  },
 ];
 
 export default function UserAddress() {
+  let { data, limit, page, status, count, setPage } = useAddressData();
 
-    let {
-        data,
-        limit,
-        page,
-        status,
-        count,
-        setPage
-    } = useAddressData();
+  return (
+    <LayoutOne size="large">
+      <div>
+        <TopBar />
+        <Text as="h3"> Alamat pengiriman </Text>
+        <br />
 
-    return (
-        <LayoutOne size="large">
-            <div>
-                <TopBar />
-                <Text as="h3"> Alamat pengiriman </Text>
-                <br />
+        <div>
+          <Link to="alamat-pengiriman/tambah">
+            <Button>Tambah baru</Button>
+          </Link>
+          <br />
+          <br />
 
-                <div>
-                    <Link to="alamat-pengiriman/tambah">
+          <Table
+            items={data}
+            columns={columns}
+            totalItems={count}
+            page={page}
+            perPage={limit}
+            isLoading={status === "process"}
+            onPageChange={(page) => setPage(page)}
+          />
+        </div>
 
-                        <Button>
-                            Tambah baru
-                        </Button>
-
-                    </Link>
-                    <br />
-                    <br />
-
-                    <Table
-                        items={data}
-                        columns={columns}
-                        totalItems={count}
-                        page={page}
-                        perPage={limit}
-                        isLoading={status === 'process'}
-                        onPageChange={page => setPage(page)}
-                    />
-
-                </div>
-
-                {
-                    status === 'success' && !data.length
-                        ?
-                        <div className="text-center p-10">
-                            Kamu belum menambahkan alamat pengiriman. <br />
-                            <Link to="/alamat-pengiriman/tambah">
-                                <Button> Tambah Baru </Button>
-                            </Link>
-                        </div>
-                        :
-                        null
-                }
-
-            </div>
-        </LayoutOne>
-    )
-
+        {status === "success" && !data.length ? (
+          <div className="text-center p-10">
+            Kamu belum menambahkan alamat pengiriman. <br />
+            <Link to="/alamat-pengiriman/tambah">
+              <Button> Tambah Baru </Button>
+            </Link>
+          </div>
+        ) : null}
+      </div>
+    </LayoutOne>
+  );
 }
