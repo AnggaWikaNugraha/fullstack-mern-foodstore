b0d66598f1c48d35342e746562dfe2e7e203bf3d
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2021-08-23 23:10:23 +0700

    register

--- foodstore-server/app/auth/router.js
+++ foodstore-server/app/auth/router.js
@@ -1,16 +1,19 @@
-// (1) import package yang diperlukan 
-const router = require('express').Router();
-const multer = require('multer');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const controller = require('./contoller');
-passport.use(new LocalStrategy({ usernameField: 'email' }, controller.localStrategy));
+// (1) import package yang diperlukan
+const router = require("express").Router();
+const multer = require("multer");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const controller = require("./contoller");
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, controller.localStrategy)
+);
 
 // (3) buat endpoint untuk register user baru
-router.post('/register', multer().none(), controller.register);
-router.post('/login', multer().none(), controller.login)
-router.get('/me', controller.me);
-router.post('/logout', controller.logout);
+// multer untuk jenis form-data
+router.post("/register", multer().none(), controller.register);
+router.post("/login", multer().none(), controller.login);
+router.get("/me", controller.me);
+router.post("/logout", controller.logout);
 
 // (4) export router
 module.exports = router;
