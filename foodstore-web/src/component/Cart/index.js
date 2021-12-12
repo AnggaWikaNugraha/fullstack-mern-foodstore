019b4bc9cc13cba105f5800e82c283b411b886b3
AnggaWikaNugraha <anggawika18@gmail.com>
2021-12-12 11:58:16 +0700

    add styled component

--- foodstore-web/src/component/Cart/index.js
+++ foodstore-web/src/component/Cart/index.js
@@ -7,19 +7,20 @@ import { sumPrice } from '../../utils/sum-price';
 import { arrayOf, string, shape, oneOfType, number, func } from 'prop-types';
 import { CardItem, Button, Text } from 'upkit';
 import { config } from '../../config';
+import styled from '@emotion/styled'
 export default function Cart({ items, onItemInc, onItemDec, onCheckout }) {
 
     let total = sumPrice(items);
 
     return <div>
 
-        {!items.length ? <div className="text-center text-sm text-red-900"> belum ada items di keranjang </div> : null}
+        {!items.length ? <Wrapp> belum ada items di keranjang </Wrapp> : null}
 
         <div className="text-3xl flex items-center text-red-700">
             <FaCartPlus />
             <div className="ml-2">
                 Keranjang
-          </div>
+            </div>
         </div>
 
         <Text as="h5"> Total: {formatRupiah(total)} </Text>
@@ -60,7 +61,15 @@ Cart.propTypes = {
         name: string.isRequired,
         qty: oneOfType([string, number]).isRequired
     })),
-    onItemInc: func, // (1)
-    onItemDec: func, // (2)
+    onItemInc: func,
+    onItemDec: func,
     onCheckout: func,
 }
+
+const Wrapp = styled('div')((props) => ({
+    backgroundColor: 'brown',
+    padding: '10px',
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: '10px'
+}))
